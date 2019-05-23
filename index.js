5f5bed87539a00d9437c493318ed73506d23f4d9
Jonathan Skelton <jonathan.skelton@gmail.com>
2019-05-23 13:45:18 -0400

    404 is the only requirement left

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
-let count = 0;
+let breed = "";
 let message = [];
 let HTML = "";
 
 function getDogImage() {
-  fetch(`https://dog.ceo/api/breeds/image/random/${count}`)
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`, {
+    })
     .then(response => response.json())
     .then(responseJson => 
       imageSrc(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .catch(error => alert('This breed is not available.'));
 }
 
 function imageSrc(responseJson){
   message = responseJson.message;
-  displayImages();
-}
-
-function displayImages() {
-  for (i = 0; i < message.length; i++){
-    HTML += `<img src="${message[i]}" class="results-img">`;
-  }
   displayResults();
 }
 
 function displayResults() {
-  $('.results-img').replaceWith(HTML);
+  $('.results-img').replaceWith(`<img src="${message}" class="results-img">`);
   $('.results').removeClass('hidden');
 }
 
 function watchForm() {
   $('form').submit(event => {
-    count = $(":input").val();
+    breed = $(":input").val();
     event.preventDefault();
     getDogImage();
   });
 }
 
-
 $(function() {
   console.log('App loaded! Waiting for submit!');
-  var $select = $(".count");
-  for (i=1;i<=50;i++){
-      $select.append($('<option></option>').val(i).html(i))
-  }
   watchForm();
 });
